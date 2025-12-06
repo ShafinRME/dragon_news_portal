883b7d0e0278cee49dc1fb47f16b6b6ff5959a04
ShafinRME <shafin.rmedu@gmail.com>
2025-12-06 23:17:08 +0600

    Navbar design is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import user from "../assets/user.png";
+import { NavLink } from 'react-router';
+
 const Navbar = () => {
     return (
-        <div>
-            Nvabar
+        <div className='flex justify-between items-center'>
+            <div></div>
+            <div className="nav flex gap-5 text-accent">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/career">Career</NavLink>
+            </div>
+            <div className="login-btn flex gap-5">
+                <img src={user} alt="" />
+                <button className='btn btn-primary px-10'>Login</button>
+            </div>
         </div>
     );
 };
